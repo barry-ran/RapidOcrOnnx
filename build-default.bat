db7193a90da158db16f6dda5269c5ee406fb23f2
rankun <870709864@qq.com>
2026-03-19 11:03:17 +0800

    feat: windows x86

--- build-default.bat
+++ build-default.bat
@@ -22,15 +22,15 @@ cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . --config Release --target install
 popd
 
-:: mkdir win-BIN-CPU-Win32
-:: pushd win-BIN-CPU-Win32
-:: cmake -T "%VS_VER%,host=x64" -A "Win32" ^
-::   -DCMAKE_INSTALL_PREFIX=install ^
-::   -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="BIN" ^
-::   -DOCR_BUILD_CRT="True" -DOCR_ONNX="CPU" ..
-:: cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-:: cmake --build . --config Release --target install
-:: popd
+mkdir win-BIN-CPU-Win32
+pushd win-BIN-CPU-Win32
+cmake -T "%VS_VER%,host=x64" -A "Win32" ^
+  -DCMAKE_INSTALL_PREFIX=install ^
+  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="BIN" ^
+  -DOCR_BUILD_CRT="True" -DOCR_ONNX="CPU" ..
+cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+cmake --build . --config Release --target install
+popd
 
 :: mkdir win-JNI-CPU-x64
 :: pushd win-JNI-CPU-x64
@@ -62,15 +62,15 @@ cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . --config Release --target install
 popd
 
-:: mkdir win-CLIB-CPU-Win32
-:: pushd win-CLIB-CPU-Win32
-:: cmake -T "%VS_VER%,host=x64" -A "Win32" ^
-::   -DCMAKE_INSTALL_PREFIX=install ^
-::   -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="CLIB" ^
-::   -DOCR_BUILD_CRT="True" -DOCR_ONNX="CPU" ..
-:: cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-:: cmake --build . --config Release --target install
-:: popd
+mkdir win-CLIB-CPU-Win32
+pushd win-CLIB-CPU-Win32
+cmake -T "%VS_VER%,host=x64" -A "Win32" ^
+  -DCMAKE_INSTALL_PREFIX=install ^
+  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="CLIB" ^
+  -DOCR_BUILD_CRT="True" -DOCR_ONNX="CPU" ..
+cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+cmake --build . --config Release --target install
+popd
 
 mkdir win-CSTATICLIB-CPU-x64
 pushd win-CSTATICLIB-CPU-x64
@@ -82,14 +82,14 @@ cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . --config Release --target install
 popd
 
-:: mkdir win-CSTATICLIB-CPU-Win32
-:: pushd win-CSTATICLIB-CPU-Win32
-:: cmake -T "%VS_VER%,host=x64" -A "Win32" ^
-::   -DCMAKE_INSTALL_PREFIX=install ^
-::   -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="CSTATICLIB" ^
-::   -DOCR_BUILD_CRT="True" -DOCR_ONNX="CPU" ..
-:: cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-:: cmake --build . --config Release --target install
-:: popd
+mkdir win-CSTATICLIB-CPU-Win32
+pushd win-CSTATICLIB-CPU-Win32
+cmake -T "%VS_VER%,host=x64" -A "Win32" ^
+  -DCMAKE_INSTALL_PREFIX=install ^
+  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="CSTATICLIB" ^
+  -DOCR_BUILD_CRT="True" -DOCR_ONNX="CPU" ..
+cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+cmake --build . --config Release --target install
+popd
 
 @ENDLOCAL
